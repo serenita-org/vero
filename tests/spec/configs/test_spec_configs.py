11e94f358bd7f6ef33e6392ebf8a2202622b624c
eth2353 <70237279+eth2353@users.noreply.github.com>
2025-04-06 01:07:20 +0200

    Set `ELECTRA_FORK_EPOCH` for mainnet

--- tests/spec/configs/test_spec_configs.py
+++ tests/spec/configs/test_spec_configs.py
@@ -1,5 +1,4 @@
 import pytest
-import remerkleable.core
 
 from spec.configs import Network, get_network_spec
 
@@ -9,9 +8,4 @@ from spec.configs import Network, get_network_spec
     argvalues=[network for network in Network if network != Network.CUSTOM],
 )
 def test_get_network_spec(network: Network) -> None:
-    if network in (Network.MAINNET,):
-        # Electra fork epoch not defined in config
-        with pytest.raises(remerkleable.core.ObjParseException):
-            get_network_spec(network)
-    else:
-        _ = get_network_spec(network=network)
+    _ = get_network_spec(network=network)
