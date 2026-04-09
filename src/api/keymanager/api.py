84b9d6e622c3ad59c23529240ca7d36ae80cf015
eth2353 <70237279+eth2353@users.noreply.github.com>
2026-04-09 21:42:39 +0200

    VERO-08 Enforce Keymanager API token file owner-only file permissions

--- src/api/keymanager/api.py
+++ src/api/keymanager/api.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import os
+import stat
 import sys
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -20,17 +21,40 @@ if TYPE_CHECKING:
     from providers import Keymanager
 
 
-def _get_bearer_token_value(cli_args: CLIArgs) -> str:
-    # Check if the filepath exists - if it does, read the token value from it
-    fp = cli_args.keymanager_api_token_file_path
-    if fp.exists():
-        with Path.open(fp) as f:
-            return f.read().strip()
+def _ensure_bearer_token_file_permissions(fp: Path) -> None:
+    permissions = stat.S_IMODE(fp.stat().st_mode)
+    if permissions != 0o600:
+        raise PermissionError(
+            f"Bearer token file {fp} must have permissions set to 0o600",
+        )
 
-    # If it doesn't, generate a value and store it in the file.
-    bearer_token_value = os.urandom(32).hex()
-    with Path.open(fp, "w") as f:
-        f.write(bearer_token_value)
+
+def _read_bearer_token_value(fp: Path) -> str:
+    _ensure_bearer_token_file_permissions(fp)
+    with Path.open(fp) as f:
+        return f.read().strip()
+
+
+def _write_bearer_token_value(fp: Path, value: str) -> None:
+    fd = os.open(
+        fp,
+        os.O_WRONLY | os.O_CREAT | os.O_EXCL,
+        0o600,
+    )
+    with os.fdopen(fd, "w") as f:
+        f.write(value)
+
+    fp.chmod(0o600)
+
+
+def _get_bearer_token_value(cli_args: CLIArgs) -> str:
+    fp = cli_args.keymanager_api_token_file_path
+    try:
+        return _read_bearer_token_value(fp)
+    except FileNotFoundError:
+        # If it doesn't exist, generate a value and store it in the file.
+        bearer_token_value = os.urandom(32).hex()
+        _write_bearer_token_value(fp=fp, value=bearer_token_value)
 
     return bearer_token_value
 
