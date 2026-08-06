cc1d4291c11b6c85c6af2e6f51d486749351d9ff
Luca | Serenita <70237279+eth2353@users.noreply.github.com>
2026-08-06 13:26:28 +0300

    Initialize beacon nodes proposal if present
    Add initialization for beacon nodes proposal.

--- src/providers/multi_beacon_node.py
+++ src/providers/multi_beacon_node.py
@@ -134,6 +134,10 @@ class MultiBeaconNode:
         self.logger.info("Initializing beacon nodes")
         # Initialize the connected beacon nodes - retry logic is already present inside
         await asyncio.gather(*(bn.initialize_full() for bn in self.beacon_nodes))
+        if self.beacon_nodes_proposal:
+            await asyncio.gather(
+                *(bn.initialize_full() for bn in self.beacon_nodes_proposal)
+            )
 
         while (
             len(self.initialized_beacon_nodes) < self._attestation_consensus_threshold
